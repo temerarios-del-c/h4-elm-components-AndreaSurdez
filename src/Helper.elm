6e62dc22e2145c8c1ca11c97bd0cd4e661a46cca
AndreaSurdez <up230188@alumnos.upa.edu.mx>
2026-02-19 12:23:59 -0600

    tarea 4

--- src/Helper.elm
+++ src/Helper.elm
@@ -6,6 +6,9 @@ import Html.Attributes
 
 
 -- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
+--main : Html.Html msg
+--main =
+--    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
 
 
 main : Html.Html msg
