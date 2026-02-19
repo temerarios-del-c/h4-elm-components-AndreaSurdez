d82298b1699ee8ceba18d6c63b4c8d287a05aa80
AndreaSurdez <up230188@alumnos.upa.edu.mx>
2026-02-19 12:44:03 -0600

    Tarea 4

--- src/Helper.elm
+++ src/Helper.elm
@@ -4,6 +4,19 @@ import Html
 import Html.Attributes
 
 
+
+-- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
+--main : Html.Html msg
+--main =
+--    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+
+
+module Helper exposing (..)
+
+import Html
+import Html.Attributes
+
+
 joinWords : String -> String -> String
 joinWords word1 word2 =
     word1 ++ word2
